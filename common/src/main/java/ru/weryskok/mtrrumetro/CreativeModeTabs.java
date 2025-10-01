2462562ef27d96ed51b81ad4fc119f63b4c49bb0
AurLemon <aurlemoncn@gmail.com>
2025-10-01 23:06:37 +0800

    fix(lang): 统一物品组ID命名从 russianmetro_stuff 到 stuff

--- common/src/main/java/ru/weryskok/mtrrumetro/CreativeModeTabs.java
+++ common/src/main/java/ru/weryskok/mtrrumetro/CreativeModeTabs.java
@@ -4,5 +4,5 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 
 public interface CreativeModeTabs {
-    mtr.CreativeModeTabs.Wrapper RUSSIAN_METRO_STUFF = new mtr.CreativeModeTabs.Wrapper(new ResourceLocation(Main.MOD_ID, "russianmetro_stuff"), () -> new ItemStack(Blocks.MOSCOW_OLD_TICKET_BARRIER_ENTRANCE.get()));
+    mtr.CreativeModeTabs.Wrapper RUSSIAN_METRO_STUFF = new mtr.CreativeModeTabs.Wrapper(new ResourceLocation(Main.MOD_ID, "stuff"), () -> new ItemStack(Blocks.MOSCOW_OLD_TICKET_BARRIER_ENTRANCE.get()));
 }
